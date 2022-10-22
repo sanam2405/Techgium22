edb7feecefd463a1bad2daa00ea103cc84459a9b
Manas Pratim Biswas <75798265+sanam2405@users.noreply.github.com>
2022-10-22 02:05:06 -0400

    Add files via upload

---
+++ Techgium_app.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+import sklearn
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+
+scaler = StandardScaler()
+
+#Loading Models
+
+loaded_model = pickle.load(open('trained_model.sav','rb'))
+scaler = pickle.load(open('standardized_data.pkl','rb'))
+
+def glocuse_prediction(input_data):
+    
+    input_data_as_npArray = np.asarray(input_data)
+    
+    input_data_reshaped = input_data_as_npArray.reshape(1,-1)
+    
+    std_data = scaler.transform(input_data_reshaped)
+    
+    prediction = loaded_model.predict(std_data)
+    
+    return prediction
+
+
+def main():
+    
+    #Title
+    st.title("Glucose Level Predictor")
+    
+    #Getting the input data
+    
+    VoltageLevel = st.text_input("Voltage Level")
+    
+    
+    #Code for prediction
+    
+    diagnosis = ''
+    
+    if st.button("Predict"):
+        
+        diagnosis = glucose_prediction((VoltageLevel))
+        
+    st.success(diagnosis)
+        
+        
+
+if __name__ == '__main__':
+    main()
